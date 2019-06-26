2520f658a5f9f2193fc6b9432d90e96ff241e0b9
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-06-26 12:43:33 +0300

    Update envs and env secrts logic.

--- src/constants.js
+++ src/constants.js
@@ -4,13 +4,8 @@ import fs from "fs-extra";
 // For any env variable with the value of "secret", resolve the actual value from the
 // associated secrets file. Using sync fs methods for the sake of simplicity.
 // Since this will only run once when staring the app it's OK.
-const secretsEnv = mapValues(process.env, (value, key, env) => {
-  const pathEnv = `FILEPATH_${key}`;
-  const filepath = env[pathEnv];
-
-  if (!filepath) {
-    return value;
-  }
+const secretsEnv = mapValues(process.env, (value, key) => {
+  const filepath = `/run/secrets/${key}`;
 
   if (fs.existsSync(filepath)) {
     return (fs.readFileSync(filepath, { encoding: "utf8" }) || "").trim();
@@ -19,7 +14,8 @@ const secretsEnv = mapValues(process.env, (value, key, env) => {
   return value;
 });
 
-export const PG_CONNECTION_STRING = secretsEnv.PG_CONNECTION_STRING || "";
+export const PG_CONNECTION_STRING =
+  secretsEnv.JORE_PG_CONNECTION_STRING || secretsEnv.PG_CONNECTION_STRING || "";
 export const FTP_USERNAME = secretsEnv.FTP_USERNAME || "";
 export const FTP_PASSWORD = secretsEnv.FTP_PASSWORD || "";
 export const FTP_HOST = secretsEnv.FTP_HOST || "";
