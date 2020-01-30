3f1fe67902a4b20b74448a3f4351f459582bfc71
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-01-30 08:48:38 +0200

    Add deploy scripts for tags.

---
+++ deploy-nonprod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Builds and deploys all images for the Azure environments
+
+ORG=${ORG:-hsldevcom}
+
+for TAG in latest dev stage; do
+  DOCKER_IMAGE=$ORG/jore-history-graphql-import:${TAG}
+
+  docker build -t $DOCKER_IMAGE .
+  docker push $DOCKER_IMAGE
+done
